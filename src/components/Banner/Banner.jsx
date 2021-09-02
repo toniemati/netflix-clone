19eee0e46eb50fdce3ef44ae9203b3e585922311
toniemati <matikas2000@gmail.com>
2021-09-02 18:51:17 +0200

    banner completed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,6 +6,10 @@ import requests from '../../requests';
 const Banner = () => {
   const [movie, setMovie] = useState({});
 
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await tmdb.get(requests.fetchNetflixOriginals);
@@ -16,10 +20,19 @@ const Banner = () => {
   }, []);
 
   return (
-    <header className="banner"> 
-      {/* title */}
-      {/* div -> 2 buttons */}
-      {/* description */}
+    <header className="banner" style={{backgroundPosition: 'center center', backgroundSize: 'cover', backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`}}>
+      <div className="banner__contents">
+        <h1 className="banner__title">{movie?.title ?? movie?.name ?? movie?.original_name}</h1>
+
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 200)}
+        </h1>
+      </div>
     </header>
   )
 }
